40ef41ce620ff6e1f0b2c85f8af2d9ea74d87d26
MohcineDev <mouhcinedev@gmail.com>
2021-01-04 21:30:43 +0000

    add footer component for all pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import details from './components/details/details';
+import Footer from './components/Footer/Footer';
 import Navbar from "./components/Navbar/Navbar";
 import Home from './components/pages/Home';
 
 function App() {
+
   return <>
     <BrowserRouter>
       <Navbar />
       <Switch>
         <Route path='/' exact component={Home} />
+        <Route path='/details/:id' component={details} />
       </Switch>
+      <Footer />
+
     </BrowserRouter>
   </>
 }
