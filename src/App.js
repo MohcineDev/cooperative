71c8cb0c771fc7414d8448d3b94fdf53ce766c69
MohcineDev <mouhcinedev@gmail.com>
2021-01-06 21:54:24 +0000

    some updates

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import details from './components/details/details';
 import Footer from './components/Footer/Footer';
 import Navbar from "./components/Navbar/Navbar";
+import Faq from './components/pages/FAQ';
 import Home from './components/pages/Home';
+import Members from './components/pages/Members/Members';
+import Privacy from './components/pages/Privacy';
 
 function App() {
 
@@ -11,6 +14,9 @@ function App() {
       <Navbar />
       <Switch>
         <Route path='/' exact component={Home} />
+        <Route path='/faq' component={Faq} />
+        <Route path='/privacy' component={Privacy} />
+        <Route path='/members' component={Members} />
         <Route path='/details/:id' component={details} />
       </Switch>
       <Footer />
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,31 +2,24 @@ import React from 'react'
 import './hero.css'
 
 function Hero() {
-
-    // fetch('data.json')
-    // .then(function (response) {
-    //   console.log(response)
-    //   return response.json();
-    // })
-    // .then(function (myJson) {
-    //   console.log(myJson);
-    // });
-
-    const toBottom = () => { 
+ 
+    const toBottom = () => {
         window.scrollTo(
             {
-                behavior:'smooth',
-                top:window.innerHeight
+                behavior: 'smooth',
+                top: window.innerHeight
             }
         )
     }
     return (
-        <div className='container'>
-            <div className="content">
-                <h1>A Cooperative Title</h1>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum reiciendis repellat aut officia ut ipsa rem harum commodi itaque, corrupti eum est a voluptas maxime voluptate beatae eveniet magni sed?</p>
-                <button>Explore</button>
-                <div onClick={toBottom} className="scroll"></div>
+        <div className="hero">
+            <div className='container'>
+                <div className="content">
+                    <h1>A Cooperative Title</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum reiciendis repellat aut officia ut ipsa rem harum commodi itaque, corrupti eum est a voluptas maxime voluptate beatae eveniet magni sed?</p>
+                    <button>Explore</button>
+                    <div onClick={toBottom} className="scroll"></div>
+                </div>
             </div>
         </div>
     )
