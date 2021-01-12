61bbc5ca67d03cc0000bfb4f52d9e0a104997812
MohcineDev <mouhcinedev@gmail.com>
2021-01-12 18:23:23 +0000

    add product imgs carousel

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,9 +3,56 @@ import { Link } from 'react-router-dom'
 import './product.css'
 
 function Product(props) {
+    let i = 1
+
+    function goLeft(e) {
+        i--
+        move(e.target.parentElement.children[0].children[0], e.target)
+        e.target.disabled = true
+    }
+
+    function goRight(e) {
+        i++
+        move(e.target.parentElement.children[0].children[0], e.target)
+        e.target.disabled = true
+    }
+
+    function move(wrap, btn) {
+        wrap.style.transition = 'transform 0.5s ease-out'
+        wrap.style.transform = `translateX(${i * - 100}%)`
+        adjust(wrap, btn)
+    }
+
+    function adjust(wrap, btn) {
+        wrap.ontransitionend = () => {
+
+            if (wrap.children[i].id === 'f') {
+                wrap.style.transition = "none"
+                i = wrap.children.length - 2
+                wrap.style.transform = `translateX(${i * - 100}%)`
+            } else if (wrap.children[i].id === 'l') {
+                wrap.style.transition = 'none'
+                i = 1
+                wrap.style.transform = `translateX(${i * - 100}%)`
+            }
+            btn.disabled = false
+        }
+    }
+
     return (
         <div className="product">
-            <img src={"../../imgs/" + props.img} alt="" />
+            <div className="container">
+                <div className="wrapper">
+                    <img src={"../../imgs/" + props.img} id="f" alt="" />
+                    <img src={"../../imgs/" + props.img} alt="" />
+                    <img src={"../../imgs/" + props.img} alt="" />
+                    <img src={"../../imgs/" + props.img} alt="" />
+                    <img src={"../../imgs/" + props.img} id="l" alt="" />
+                </div>
+            </div>
+            <button onClick={goLeft}></button>
+            <button onClick={goRight}></button>
+            <p>{props.name}</p>
             <p>{props.desc}</p>
             <Link to="#" >
                 <button>More</button>
