bef65a69dae02e3ee3ef4439ff352649f45bf2f4
MohcineDev <mouhcinedev@gmail.com>
2021-01-12 18:25:03 +0000

    add product component

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -5,7 +5,7 @@ import product from './data.json'
 
 function Shop() {
     window.scrollTo(0, 0)
-
+    
     return (
         <div className="shop">
             <h1>Our Cooperative Products</h1>
@@ -14,28 +14,28 @@ function Shop() {
                 <h3>Category Name</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, assumenda perferendis! Quis aspernatur repellendus, dolore, vitae blanditiis dolorem quae placeat architecto totam ipsum repellat aliquam a magni expedita quos neque!</p>
                 <div className="wrap">
-                    <Product desc={product[0].desc} img={product[0].img} />
-                    <Product desc={product[1].desc} img={product[1].img} />
-                    <Product desc={product[2].desc} img={product[2].img} />
-                    <Product desc={product[3].desc} img={product[3].img} />
+                    <Product desc={product[0].desc} name={product[0].name} img={product[0].img} />
+                    <Product desc={product[1].desc} name={product[1].name} img={product[1].img} />
+                    <Product desc={product[2].desc} name={product[2].name} img={product[2].img} />
+                    <Product desc={product[3].desc} name={product[3].name} img={product[3].img} />
                 </div>
                 <div className="wrap">
-                    <Product desc={product[4].desc} img={product[4].img} />
-                    <Product desc={product[5].desc} img={product[5].img} />
-                    <Product desc={product[6].desc} img={product[6].img} />
-                    <Product desc={product[7].desc} img={product[7].img} />
+                    <Product desc={product[4].desc} name={product[4].name} img={product[4].img} />
+                    <Product desc={product[5].desc} name={product[5].name} img={product[5].img} />
+                    <Product desc={product[6].desc} name={product[6].name} img={product[6].img} />
+                    <Product desc={product[7].desc} name={product[7].name} img={product[7].img} />
                 </div>
                 <h3>Another Category Name</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, assumenda perferendis! Quis aspernatur repellendus, dolore, vitae blanditiis dolorem quae placeat architecto totam ipsum repellat aliquam a magni expedita quos neque!</p>
                 <div className="wrap">
-                    <Product desc={product[8].desc} img={product[8].img} />
-                    <Product desc={product[9].desc} img={product[9].img} />
-                    <Product desc={product[10].desc} img={product[10].img} />
-                    <Product desc={product[11].desc} img={product[11].img} />
+                    <Product desc={product[8].desc} name={product[8].name} img={product[8].img} />
+                    <Product desc={product[9].desc} name={product[9].name} img={product[9].img} />
+                    <Product desc={product[10].desc} name={product[10].name} img={product[10].img} />
+                    <Product desc={product[11].desc} name={product[11].name} img={product[11].img} />
                 </div>
                 <div className="wrap">
-                    <Product desc={product[8].desc} img={product[8].img} />
-                    <Product desc={product[9].desc} img={product[9].img} />
+                    <Product desc={product[12].desc} name={product[12].name} img={product[12].img} />
+                    <Product desc={product[13].desc} name={product[13].name} img={product[13].img} />
                 </div>
             </div>
         </div>
