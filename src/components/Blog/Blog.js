762ccadee12016ea6ccb49b71bd55e0d65a09d5c
MohcineDev <Mouhcinedev@gmail.com>
2021-01-31 19:31:06 +0000

    add blog component

---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./blog.css"
+
+function Blog({ id, title, short, date }) {
+    const blogDate =new Date(date) 
+
+    return (
+        <Link to={`blog/${id}`}>
+            <div>
+                <h2>{title}</h2>
+                <p>{short}</p>
+                <div>
+                    <p>{blogDate.toDateString()}</p>
+                </div>
+            </div>
+        </Link>
+    )
+}
+
+export default Blog
