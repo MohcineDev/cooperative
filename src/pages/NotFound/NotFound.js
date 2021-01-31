da1f5a184fcbf5acd5f4a3e64c4166c894125611
MohcineDev <Mouhcinedev@gmail.com>
2021-01-31 19:37:45 +0000

    add height

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import './notFound.css'
 function NotFound(props) {
     return (
-        <div>
+        <div className="NotFound">
             <h1>404</h1>
             <h2>Not Found!! {props.location.pathname}</h2>
         </div>
