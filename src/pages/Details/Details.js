404a02269881e968d264663cfba4825cab5204d7
MohcineDev <Mouhcinedev@gmail.com>
2021-05-23 17:38:17 +0000

    styled components

--- src/pages/Details/Details.js
+++ src/pages/Details/Details.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react'
 import './g_details.css'
 import data from './data.json'
+import { DetailsSec, H1, Row, Img } from './styled.js'
+
 
 function Details(props) {
 
@@ -9,49 +11,43 @@ function Details(props) {
         window.scrollTo(0, 0)
 
     }, [])
+
     const key = parseInt(props.match.params.id)
     let list = data.find(item => item.id === key)
 
     return (
-        <section className="details">
-            <h1>{list.title}</h1>
-            <div className="d-container">
-                <div className="d-row">
-                    <div className="img">
-                        <img src={list.img1} alt="" />
-                    </div>
-
+        <DetailsSec>
+            <H1>{list.title}</H1>
+            <div>
+                <Row>
+                    <Img src={list.img1} alt="" />
                     <div>
                         <h2>Sub title here</h2>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut eum asperiores ullam quidem unde consequatur culpa eius dicta itaque hic totam impedit nostrum eligendi, minus veniam dolorum! Dignissimos, corrupti impedit!
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet, aliquam sint. Et sed reprehenderit voluptatem dicta culpa expedita fuga laborum minima sit, corporis dolores ut dolor, necessitatibus ipsa alias quibusdam.
                             </p>
                     </div>
-                </div>
-                <div className="d-row">
+                </Row>
+                <Row>
                     <div>
                         <h2>Sub title here</h2>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut eum asperiores ullam quidem unde consequatur culpa eius dicta itaque hic totam impedit nostrum eligendi, minus veniam dolorum! Dignissimos, corrupti impedit!
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet, aliquam sint. Et sed reprehenderit voluptatem dicta culpa expedita fuga laborum minima sit, corporis dolores ut dolor, necessitatibus ipsa alias quibusdam.
                             </p>
                     </div>
-                    <div className="img">
-                        <img   src={list.img2} alt="" />
-                    </div>
-                </div>
-                <div className="d-row">
-                    <div className="img">
-                        <img src={list.img3} alt="" />
-                    </div>
+                    <Img src={list.img2} alt="" />
+                </Row>
+                <Row last>
+                    <Img src={list.img3} alt="" />
                     <div>
                         <h2>Sub title here</h2>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut eum asperiores ullam quidem unde consequatur culpa eius dicta itaque hic totam impedit nostrum eligendi, minus veniam dolorum! Dignissimos, corrupti impedit!
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet, aliquam sint. Et sed reprehenderit voluptatem dicta culpa expedita fuga laborum minima sit, corporis dolores ut dolor, necessitatibus ipsa alias quibusdam.
                             </p>
                     </div>
-                </div>
+                </Row>
             </div>
-        </section>
+        </DetailsSec>
     )
 }
 
--- src/pages/Details/styled.js
+++ src/pages/Details/styled.js
@@ -1,5 +1,25 @@
 import styled from 'styled-components'
 
-export const Div = styled.div`
+export const DetailsSec = styled.section`
+width: 80%;
+margin: 0 auto;
+`
+
+export const H1 = styled.h1`
+text-align: center;
+text-transform: capitalize;
+`
+
+export const Row = styled.div`
+display: grid;
+grid-template-columns: repeat(2, minmax(0px, 50%));
+gap: 2rem;
+padding: 2rem 0;
+align-items: center;
+
+${props => props.last ? null : " border-bottom: 2px dashed #ddd"}
 
 `
+export const Img = styled.img`
+     width: 100%;
+     `
