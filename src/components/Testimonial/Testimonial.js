7b8dbabf83793bfa5741d2e054100149f7a8c95a
MohcineDev <Mouhcinedev@gmail.com>
2021-11-10 19:29:51 +0000

    adjust the i+1 to display last testimonial

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -9,7 +9,7 @@ function Testimonial() {
         let i = 1
         let btns = document.querySelectorAll('.btn')
         let wrapper = document.querySelector('.tes_wrapper')
-        let a = document.querySelectorAll('.tes_wrapper>div')
+        let testimonials = document.querySelectorAll('.tes_wrapper>div')
 
         const move = (e) => {
             wrapper.style.transition = 'transform 0.7s';
@@ -18,7 +18,6 @@ function Testimonial() {
         }
 
         const slide = (e) => {
-            console.log(e.target);
             if (e.target.id === "next") {
                 i++
                 move(e)
@@ -27,15 +26,14 @@ function Testimonial() {
                 i--
                 move(e)
             }
-            console.log(i);
         }
+
         wrapper.addEventListener('transitionend', () => {
-            console.log(wrapper);
-            console.log(a[i]);
-            if (a[i].id === "f") {
+            if (testimonials[i].id === "f") {
                 btns[0].style.display = 'none'
             }
-            else if (a[i + 2].id === "l") {
+            //if we reached the last testimonial
+            else if (testimonials[i + 1].id === "l") {
                 btns[1].style.display = 'none'
             }
             else {
@@ -55,15 +53,16 @@ function Testimonial() {
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non explicabo harum, dicta error earum tempore aliquid, necessitatibus dolor, sit blanditiis molestiae dolorem cumque odit. Non sit repellendus explicabo dignissimos ratione?</p>
             <div className="container">
                 <div className="tes_wrapper">
-                    <OneTes divId={data[0].divId} img={data[0].img} name={data[0].name} tagname={data[0].tagname} testimonial={data[0].testimonial} />
-                    <OneTes divId={data[1].divId} img={data[1].img} name={data[1].name} tagname={data[1].tagname} testimonial={data[1].testimonial} />
-                    <OneTes divId={data[2].divId} img={data[2].img} name={data[2].name} tagname={data[2].tagname} testimonial={data[2].testimonial} />
-                    <OneTes divId={data[3].divId} img={data[3].img} name={data[3].name} tagname={data[3].tagname} testimonial={data[3].testimonial} />
-                    <OneTes divId={data[4].divId} img={data[4].img} name={data[4].name} tagname={data[4].tagname} testimonial={data[4].testimonial} />
-                    <OneTes divId={data[5].divId} img={data[5].img} name={data[5].name} tagname={data[5].tagname} testimonial={data[5].testimonial} />
-                    <OneTes divId={data[6].divId} img={data[6].img} name={data[6].name} tagname={data[6].tagname} testimonial={data[6].testimonial} />
-                    <OneTes divId={data[7].divId} img={data[7].img} name={data[7].name} tagname={data[7].tagname} testimonial={data[7].testimonial} />
-                    <OneTes divId={data[8].divId} img={data[8].img} name={data[8].name} tagname={data[8].tagname} testimonial={data[8].testimonial} />
+                    {
+                        data.map(item =>
+                            <OneTes key={item.id}
+                                divId={item.divId}
+                                img={item.img}
+                                name={item.name}
+                                tagname={item.tagname}
+                                testimonial={item.testimonial} />
+                        )
+                    }
                 </div>
                 <button className="btn" id="prev"></button>
                 <button className="btn" id="next"></button>
