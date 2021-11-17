856e18df2cfc76c34fba566cb0aef00040f144af
MohcineDev <Mouhcinedev@gmail.com>
2021-11-17 21:38:40 +0000

    move fetch inside useEffect

--- src/pages/SingleBlog/SingleBlog.js
+++ src/pages/SingleBlog/SingleBlog.js
@@ -5,31 +5,33 @@ import Social from '../../components/Social.js'
 function SingleBlog(props) {
 
     const [blog, setBlog] = useState({})
-
-    const getBlog = async () => {
-        const dataa = await fetch(`../data.json`)
-
-        const res = await dataa.json()
-        setBlog(...res.blogs.filter(item => item.id === parseInt(props.match.params.id)))
-    }
-
+ 
     useEffect(() => {
-        window.scrollTo(0, 0)
+        //include the function here to avoid the useEffect dependencies warnings
+        async function getBlog() {
+
+            const dataa = await fetch(`../blogsData.json`)
+            const res = await dataa.json()
+            setBlog(...res.blogs.filter(item => item.id === parseInt(props.match.params.id)))
+        }
         getBlog()
+        window.scrollTo(0, 0)
         document.title = `Cooperative | ${blog.title}`
-    }, [])
+        
+    }, [props.match.params.id, blog.title])
 
     return (
-        <div className="singleblog">    
+        <div className="singleblog">
             {
-                blog.title ?
+                //if there is a title there is a blog
+                blog.title?
                     <>
-                        <h1> {blog.title}</h1>
+                        <h1>{blog.title}</h1>
                         <p>{blog.header}</p>
                         <div className="imgs">
                             {
                                 blog.imgs.map((item, index) =>
-                                    <img key={index} loading="lazy" src={item} alt="blog" />
+                                    <img key={index} src={item} alt="blog" />
                                 )
                             }
                         </div>
@@ -39,7 +41,7 @@ function SingleBlog(props) {
                             <Social color1='#ccc' color2='#fff' />
                         </div>
                     </>
-                    : <h1 className='err'>Nothing Found!!</h1>
+                    : <h1 className='err'>Sorry, Nothing Found!!</h1>
             }
         </div>
     )
